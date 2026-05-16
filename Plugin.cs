ebb8de5e0fbc5f57240db951782b2cee1bb8ce7d
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2026-05-16 15:45:04 -0500

    Version change

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace MapValueTracker
     {
         public const string PLUGIN_GUID = "MapValueTrackerPlus";
         public const string PLUGIN_NAME = "Map Value Tracker Plus";
-        public const string PLUGIN_VERSION = "1.2.0";
+        public const string PLUGIN_VERSION = "1.2.1";
         private const float SnapshotRefreshResetTime = -100000f;
 
         public static new ManualLogSource Logger = null!;
